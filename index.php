8b35ff3ee1d93297d76491e708edfef0f2802ee8
bram.waasdorp <bram.waasdorp@FNV-LT10086.FNV.NET>
2020-05-10 11:03:40 +0200

    initiele aanpassingen alles no hardcoded

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
+ * 10-5-2020 eerste aanpassingen richting onepage
  
 */
 
@@ -82,7 +83,7 @@ echo '<!-- base is ' . $this->getBase() .' $templatestyleid ='.$templatestyleid 
 
 // Add extra metadata
 $this->setMetaData( 'X-UA-Compatible', 'IE=edge', true ); // http-equiv = true 
-$this->setMetaData( 'viewport', 'width=device-width, initial-scale=1.0' );
+$this->setMetaData( 'viewport', 'width=device-width, initial-scale=1.0, shrink-to-fit=no' );
 // stylesheets
 $this->addStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' , array('version'=>'auto'), array('id'=>'googleapis-fonts.css'));
 // bootstrap stylesheets van cdn
@@ -106,6 +107,7 @@ else {
 $attribs = array('id'=>'template.css');
 $this->addStyleSheet('templates/' . $this->template . '/css/' . $wsaCssFilename , array('version'=>$wsaTime), $attribs);
 
+$this->addStyleSheet('templates/' . $this->template . '/css/' . 'scrolling-nav.css' , array('version'=>$wsaTime), $attribs);
 // Add JavaScript 
 
 //HTMLHelper::_('jquery.framework');  // to be sure that jquery is loaded before dependent javascripts
@@ -190,7 +192,7 @@ class="site-grid site <?php echo $option
 ?>"
 
 >
-
+<body id="page-top">
 <!-- Begin Container-->
 	<div id="wrapper" class="container">
 <?php if ($bg1Image > " " )
@@ -213,7 +215,13 @@ class="site-grid site <?php echo $option
 }?>
 
 <div id="wrapper1">
-		<!-- Begin Header-->
+
+<?php 
+/*  gegenereerde header tijdelijk uitschakelen
+
+
+
+		<!-- Begin Header  -->
 		<div class="header">
 			<div class="header-inner container-content<?php echo $hi_mods; ?>">
 				<?php if ($this->countModules('position-0')): ?>
@@ -256,7 +264,59 @@ class="site-grid site <?php echo $option
 				<div class="clearfix"></div>
 			</div><!--End Header-Inner-->
 		</div><!--End Header-->
+		
+*/ ?>		
+
+<!--  tijdelijk hard coded nav -->
+
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+		
+		
+<!--  einde tijdelijk hard coded nav -->
+
+<!--  main content menusturing door DIVs met ID -->
+	<section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>About this page</h2>
+          <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
+          <ul>
+            <li>Clickable nav links that smooth scroll to page sections</li>
+            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
+            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  
 		<!-- Begin Container content-->
+<section id="services" class="bg-light">
+
 		<div class="container-content<?php echo $cnt_mods; ?>">
 		    	<?php if ($this->countModules('position-1')): ?>
 	            	    <jdoc:include type="modules" name="position-1" style="none" />
@@ -291,6 +351,22 @@ class="site-grid site <?php echo $option
 			</div><!--End Row-->
          
           </div><!--End Container Content-->
+          
+          </section>
+          
+      <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Contact us</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+        </div>
+      </div>
+    </div>
+  </section>    
+          
+    <!--  einde main content menusturing door DIVs met ID -->
+          
 	<!-- Begin Footer -->
 	<div class="footer">
 			<hr />
@@ -307,5 +383,17 @@ class="site-grid site <?php echo $option
 	<![endif]-->
 	<?php endif; ?>
 	<jdoc:include type="modules" name="debug" style="none" />
+	
+	  <!-- Bootstrap core JavaScript -->
+  <!--  script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script -->
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+	
+	
 </body>
 </html>
