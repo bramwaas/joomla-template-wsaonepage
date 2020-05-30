aad8dda4bb579d312157959b12b9e15c68fb5da9
bram.waasdorp <bram.waasdorp@FNV-LT10086.FNV.NET>
2020-05-30 13:48:14 +0200

    begin sections op

--- html/mod_menu/wsaonepagebs4.php
+++ html/mod_menu/wsaonepagebs4.php
@@ -204,3 +204,42 @@ if (  $document->countModules('navbar-right')) {
 echo '</div><!--/div container-fluid --></' . $moduleTag . '><!--End navbar-->'. PHP_EOL;
 
 ?>
+
+
+<?php 
+// voorlopig extra acties voor onepage sections
+echo '<!-- onepage sections uit menu -->'. PHP_EOL;
+foreach ($list as $i => &$item) {
+    echo '<!-- item->type=' , $item->type , ' item->level=' , $item->level ,  ' $item->title=' , $item->title , ' $item->flink=' , $item->flink,  ' -->', PHP_EOL;;  
+    
+    
+    
+}
+
+/*
+ * 	<section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>About this page</h2>
+          <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
+          <ul>
+            <li>Clickable nav links that smooth scroll to page sections</li>
+            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
+            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+ * 
+ * 
+ * 
+ */
+
+echo '<!-- einde onepage sections uit menu -->'. PHP_EOL;
+
+?>
