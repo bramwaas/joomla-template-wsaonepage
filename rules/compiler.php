b8b7623e0221b30d0aaacd38c8ab5bdeb01f14cd
Bram Waasdorp <bram@waasdorpsoekhan.nl>
2020-09-06 18:14:07 +0200

    Update compiler.php
    Changed namespace and directory from Leafo to ScssPhp

--- rules/compiler.php
+++ rules/compiler.php
@@ -12,8 +12,8 @@
 	*/
  
 defined('_JEXEC') or die('caught by _JEXEC');
-use Leafo\ScssPhp\Compiler;
-use Leafo\ScssPhp\Server;
+use ScssPhp\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\Server;
 
 use Joomla\CMS\Factory;   
 use Joomla\CMS\Uri\Uri;
@@ -72,9 +72,9 @@ if  (htmlspecialchars($value) == '1')
 
 { /* creeren en compileren */
 
-// scss compiler van leafo http://leafo.github.io/scssphp/
-require_once "leafo/scss.inc.php";
-require_once "leafo/src/Server.php";
+// scss compiler van ScssPhp https://scssphp.github.io/scssphp/ ( was leafo http://leafo.github.io/scssphp/ )
+require_once "scssphp/scss.inc.php";
+require_once "scssphp/src/Server.php";
 
 
 $scss = new Compiler();
